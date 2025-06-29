49edbcf236acc2e6d3608ef370806e44a8f0bdd4
harisalsheikh <harisalsheikh@hotmail.com>
2025-06-29 16:15:49 +0800

    Updated AWS Lambda Python Code

--- AWS Lambda/GetCounter.py
+++ AWS Lambda/GetCounter.py
@@ -1,20 +1,38 @@
 import boto3
-from boto3.dynamodb.conditions import Key
+import json
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("ResumeVisitorCounter")
+
 
 def lambda_handler(event, context):
-    
-    client = boto3.resource("dynamodb")
-    table = client.Table("CloudResumeChallenge")
-    
-    # Get all items 
-    scan_table = table.scan()["Items"]
-    
-    # Get Counter Item from table
-    
-    #empty list to store content of the "Counter" Item
-    item = []
-    
-    for items in scan_table:
-        item.append(items["Counter"])
-        
-    return item
+    try:
+        print("Event received:", event)
+
+        response = table.update_item(
+            Key={"id": "visitor-count"},
+            UpdateExpression="SET #c = if_not_exists(#c, :start) + :inc",
+            ExpressionAttributeNames={"#c": "count"},
+            ExpressionAttributeValues={":inc": 1, ":start": 0},
+            ReturnValues="UPDATED_NEW",
+        )
+
+        new_count = response["Attributes"]["count"]
+        print("New count is:", new_count)
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps({"count": int(new_count)}),
+        }
+
+    except Exception as e:
+        print("Error occurred:", str(e))
+        return {
+            "statusCode": 500,
+            "headers": {"Access-Control-Allow-Origin": "*"},
+            "body": json.dumps({"error": "Could not update counter"}),
+        }
